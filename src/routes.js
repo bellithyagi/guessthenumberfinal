7fac846237cacd0bfde56dfa6f76800f4eb42314
bellithyagi <34711707+bellithyagi@users.noreply.github.com>
2018-01-07 20:10:47 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
-
+import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import Signup from './components/signup/signup.component';
 import Signin from './components/signin/signin.component';
 import Landingpage from './components/landingpage/landingpage.component';
 
 export default (
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Signup}/>
-            <Route path="/signin" component={Signin}/>
-            <Route path="/guessnumber" component={Landingpage}/>
-        </Route>
-    </Router>
+    <Route path="/" component={App}>
+        <IndexRoute component={Signup}/>
+        <Route path="/signin" component={Signin}/>
+        <Route path="/guessnumber" component={Landingpage}/>
+    </Route>
 );
